0d5536d917823d5619b96b6236b1af52ae8099ce
maximovsergey <maximov.sergey.1985@gmail.com>
2019-01-30 13:57:20 +0500

    Пишем метод получения данных по ссылке

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Пустой проект");
+const getResource = async (url) => {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}` +
+            `, received ${res.status}`);
+    }
+    return await res.json();
+}
+getResource('https://swapi.co/api/people/11656/').then((body) => {
+    console.log('////////// body ', body);
+}).catch((err) => {
+    console.error('Could not fetch ', err);
+})
+// await - будем ждать пока результат промиса не будет доступен
